a7dd550ecf9aea6e0f9f6fcaf9a8eeb7fdb5657b
debugger77 <devhr78@gmail.com>
2023-08-12 11:54:24 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//components
+import Home from "./component/Home";
+
+import DataProvider from "./component/context/DataProvider";
+
+function App() {
+  return (
+    <DataProvider>
+      <Home />
+    </DataProvider>
+  );
+}
+
+export default App;
